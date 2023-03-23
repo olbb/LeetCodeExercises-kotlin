4802e9f37dbc4aaef688ca1af58de8454dc795e0
JuanLv <5554102+olbb@users.noreply.github.com>
2023-03-23 11:35:36 +0800

    Case9

--- src/test/kotlin/TestCase.kt
+++ src/test/kotlin/TestCase.kt
@@ -233,6 +233,28 @@ class TestCase {
     }
 
     @Test
+    fun testCase9() {
+        //示例 1：
+        //输入：x = 121
+        //输出：true
+        //示例 2：
+        //输入：x = -121
+        //输出：false
+        //解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+        //示例 3：
+        //输入：x = 10
+        //输出：false
+        //解释：从右向左读, 为 01 。因此它不是一个回文数。
+        //示例 4：
+        //输入：x = -101
+        //输出：false
+        assert(isPalindrome(121))
+        assert(!isPalindrome(-121))
+        assert(!isPalindrome(10))
+        assert(!isPalindrome(-101))
+    }
+
+    @Test
     fun testCase20() {
         //示例 1：
         //输入：s = "()"
