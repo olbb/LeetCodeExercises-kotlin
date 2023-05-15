b01161317c2701c510ee6527a9f3455ae898a68d
JuanLv <5554102+olbb@users.noreply.github.com>
2023-05-15 16:51:43 +0800

    Case12

--- src/test/kotlin/TestCase.kt
+++ src/test/kotlin/TestCase.kt
@@ -292,6 +292,38 @@ class TestCase {
     }
 
     @Test
+    fun testCase12() {
+        //示例 1:
+        //
+        //输入: num = 3
+        //输出: "III"
+        //示例 2:
+        //
+        //输入: num = 4
+        //输出: "IV"
+        //示例 3:
+        //
+        //输入: num = 9
+        //输出: "IX"
+        //示例 4:
+        //
+        //输入: num = 58
+        //输出: "LVIII"
+        //解释: L = 50, V = 5, III = 3.
+        //示例 5:
+        //
+        //输入: num = 1994
+        //输出: "MCMXCIV"
+        //解释: M = 1000, CM = 900, XC = 90, IV = 4.
+        assert(intToRoman(3) == "III")
+        assert(intToRoman(4) == "IV")
+        assert(intToRoman(9) == "IX")
+        assert(intToRoman(58) == "LVIII")
+        assert(intToRoman(1994) == "MCMXCIV")
+
+    }
+
+    @Test
     fun testCase20() {
         //示例 1：
         //输入：s = "()"
